0482aeb17b9b541697817c81dd6e880a835a1050
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-12-05 14:38:39 +0000

    Updated a function to be easyier to understand

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -38,28 +38,24 @@ namespace csharp
 
         private void UpdateQuailtyIfSellInIsZero(int i)
         {
-            if (Items[i].SellIn < 0)
+            if (Items[i].SellIn >= 0) return;
+    
+            if (Items[i].Name == "Aged Brie")
             {
-                if (Items[i].Name != "Aged Brie")
+                if (Items[i].Quality < 50)
                 {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
+                    Items[i].Quality = Items[i].Quality + 1;
                 }
-                else
+            }
+            else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                Items[i].Quality = 0;
+            }
+            else
+            {
+                if (Items[i].Quality > 0)
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+                    Items[i].Quality = Items[i].Quality - 1;
                 }
             }
         }
