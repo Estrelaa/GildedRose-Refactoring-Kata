3f1b1dc45169b3ef91072512dd8cc41de97eb185
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-12-05 11:22:08 +0000

    Fixed test file to work and extracted the adding of items into a function

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -9,6 +9,26 @@ namespace csharp
         {
             Console.WriteLine("OMGHAI!");
 
+            IList<Item> Items = AddItems();
+
+            var app = new GildedRose(Items);
+
+
+            for (var i = 0; i < 31; i++)
+            {
+                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine("name, sellIn, quality");
+                for (var j = 0; j < Items.Count; j++)
+                {
+                    System.Console.WriteLine(Items[j]);
+                }
+                Console.WriteLine("");
+                app.UpdateQuality();
+            }
+        }
+
+        private static IList<Item> AddItems()
+        {
             IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
@@ -36,21 +56,7 @@ namespace csharp
 				// this conjured item does not work properly yet
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
-
-            var app = new GildedRose(Items);
-
-
-            for (var i = 0; i < 31; i++)
-            {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
-                {
-                    System.Console.WriteLine(Items[j]);
-                }
-                Console.WriteLine("");
-                app.UpdateQuality();
-            }
+            return Items;
         }
     }
 }
